834dfc6eb01528eb275fbb6919c03b1bb796b5ba
anvqcs <anvqcs@gmail.com>
2024-08-06 21:37:38 +0700

    anvqcs: add template

---
+++ Ecommerce/ViewComponents/MenuProductCategoryViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Ecommerce.Data;
+using Ecommerce.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecommerce.ViewComponents
+{
+    [ViewComponent(Name = "MenuProductCategory")]
+    public class MenuProductCategoryViewComponent: ViewComponent 
+    {
+        private readonly EcommerceContext _context;
+
+        public MenuProductCategoryViewComponent(EcommerceContext context) 
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            var item = _context.ProductCategories.Select(item => new MenuProductCategoryVM
+            {
+                Id =  item.ProductCategoryId, Name = item.Title, Count = item.Products.Count
+            });
+            return View(item);
+        }
+    }
+}
---
+++ Ecommerce/ViewModels/MenuProductCategoryVM.cs
@@ -0,0 +1,9 @@
+﻿namespace Ecommerce.ViewModels
+{
+    public class MenuProductCategoryVM
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public int Count { get; set; }
+    }
+}
